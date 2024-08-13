6472dc14b9778de4615079a80373e27bda7b0e71
isi07 <65848350+isi07@users.noreply.github.com>
2024-08-13 16:48:27 +0200

    Revert "fix #394"

--- admin/blockly.js
+++ admin/blockly.js
@@ -240,7 +240,7 @@ Blockly.JavaScript['pushover'] = function(block) {
 
     const priority = parseInt(block.getFieldValue('PRIORITY'), 10);
     if (priority) {
-        text += '  priority: ' + priority + ',\n';
+        text += '  priority: ' + value + ',\n';
 
         if (priority === 2) {
             text += '  retry: 60,\n';
